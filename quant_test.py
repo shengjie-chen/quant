2893eae546d866d26c31fb64abc890440b64bdbc
chenshengjie1999@126.com <Shengjie Chen>
2022-10-29 13:51:29 +0800

    开始简化train.py

--- quant_test.py
+++ quant_test.py
@@ -45,129 +45,19 @@ from quant_utils import load_model_data, quant_model_evaluate_show_name, print_s
     generate_quant_model_baseline, generate_quant_model_selfdefine,quant_model_detect_v3tall
 import warnings
 
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='test.py')
-    parser.add_argument('--model_name', type=str, default='voc_retrain',
-                        help='which model? choose in voc_retrain | coco_origin | coco128_retrain ')
-    parser.add_argument('--data', type=str,
-                        default='data/voc.yaml', help='*.data path')
-    parser.add_argument('--batch-size', type=int, default=32,
-                        help='size of each image batch')
-
-    parser.add_argument('--task', type=str,
-                        default='test', help='train, val or test')
-
-    parser.add_argument('--one_anchor', action='store_true',
-                        help='default means use all two anchor, and add the flag means use one anchor.')
-
-    parser.add_argument('--recon_qmodel', action='store_true',
-                        help='default means load quant model,and add the flag means reconstruct quant model.')
-    parser.add_argument('--cs_self_def', action='store_true',
-                        help='calibration source:a folder including some source imgs to feed quantization model defined manually')
-    parser.add_argument('--cs_dir', type=str, default='E:/Academic_study/datasets/coco128/images/train2017',
-                        help='if cs_self_def is true, use cs_dir as calibration source')
-    parser.add_argument('--cs_use_set', type=str, default='val',
-                        help='if cs_self_def is false, use dataset as calibration source, you can choose in val and train')
-    parser.add_argument('--cs_num', type=int, default=128,
-                        help='limit img number of calibration source')
-
-    parser.add_argument('--quant_strategy', type=str, default='baseline',
-                        help='choose quant strategy in baseline/selfdefine...')
-    parser.add_argument('--save_qw', action='store_true',
-                         help='save quanted weight')
-    opt = parser.parse_args()
-    # if('recon_qmodel' in vars(opt)):
-    #     if('cs_self_def' in vars(opt)):
-    #         if('')
-    # opt.save_json |= opt.data.endswith('coco.yaml')
-    # opt.data = check_file(opt.data)  # check file
-    print(opt, type(opt))
-    warnings.filterwarnings('ignore')
-
-    model_name = opt.model_name
-    data = 'models/yolov3-tiny_voc.yaml' if model_name == 'voc_retrain' else 'models/yolov3-tiny.yaml'
-    relative_path = './models_files/' + model_name
-    float_weight = relative_path + '/weights/best.pt'
-    quant_weight = relative_path + '/yolov3tiny_quant.pth'
-
-    assert(os.path.exists(float_weight))
-    assert(os.path.exists(quant_weight))
-    print("->使用现有浮点权重：\n\t"+str(float_weight))
-    if(opt.recon_qmodel):
-        # 准备校准数据
-        if opt.cs_self_def:
-            perpare_source = opt.cs_dir
-        else:
-            dataset_yaml = 'data/' + model_name.split('_')[0]+'.yaml'
-            with open(dataset_yaml) as f:
-                data_yaml = yaml.safe_load(f)
-            perpare_source = data_yaml[opt.cs_use_set]
-        print("->重构量化权重：\n\t使用校准数据："+str(perpare_source) +
-              "\n\t图片数量："+str(opt.cs_num))
-        calibrate_dataset = LoadImages(perpare_source, img_size=416, stride=32)
-        float_model = load_float_model(float_weight)
-        
-        if(opt.quant_strategy == 'baseline'):
-            print("->baseline量化策略：")
-            quant_model = generate_quant_model_baseline(
-                float_model, calibrate_dataset, opt.cs_num)
-        else:
-            print("->selfdefine量化策略：")
-            quant_model = generate_quant_model_selfdefine(
-                float_model, calibrate_dataset, opt.cs_num)
-    else:
-        # load model only in default_qconfig strategy
-        quant_model = load_quant_model(float_weight, quant_weight)
-        print("加载现有量化权重："+str(quant_weight))
-
-        # 保存量化模型
-    if opt.save_qw:
-        state_dict = quant_model.state_dict()
-        torch.save(state_dict, './models_files/' +
-                   model_name + '/yolov3tiny_quant.pth')
-
-    # Configure
-    data_yaml = opt.data
-    if isinstance(data_yaml, str):
-        with open(data_yaml) as f:
-            data_cfg = yaml.safe_load(f)
-    check_dataset(data_cfg)  # check
-    nc = int(data_cfg['nc'])  # number of classes
-    iouv = torch.linspace(0.5, 0.95, 10)  # iou vector for mAP@0.5:0.95
-    niou = iouv.numel()
-    imgsz = 416
-    gs = 32
-    names = {k: v for k, v in enumerate(range(nc))}
-    anchors = [[10, 14, 23, 27, 37, 58], [81, 82, 135, 169, 344, 319]]
-    anchors = [anchors[1], anchors[0]]
-    # stride = [32, 16]
-    conf_thres = 0.001
-    iou_thres = 0.6  # for NMS
-    batch_size = opt.batch_size
-    device = 'cpu'
-    na = quant_model[1].model[-1].na
-    nd = 1 if opt.one_anchor else quant_model[1].model[-1].nl # number of detection layers
-    no = quant_model[1].model[-1].no
-    stride = quant_model[1].model[-1].stride
-    anchor = quant_model[1].model[-1].anchors
-    anchor_grid = quant_model[1].model[-1].anchor_grid
-
-    task = opt.task
-    dataloader = create_dataloader(data_cfg[task], imgsz, batch_size, gs, opt, pad=0.5, rect=True,
-                                   prefix=colorstr(f'{task}: '))[0]
-    best_acc, old_file = 0, None
+def quant_test(quant_model,dataloader,nc ,iouv,niou,names,conf_thres,iou_thres,device,nd,na,no,stride,anchor_grid):
+    # best_acc, old_file = 0, None
     s = ('%20s' + '%12s' * 6) % ('Class', 'Images',
                                  'Labels', 'P', 'R', 'mAP@.5', 'mAP@.5:.95')
     p, r, f1, mp, mr, map50, map, t0, t1 = 0., 0., 0., 0., 0., 0., 0., 0., 0.
-    t_begin = time.time()
-    test_loss = torch.zeros(3, device=device)
+    # t_begin = time.time()
+    # test_loss = torch.zeros(3, device=device)
     stats, ap, ap_class = [], [], []
     correct = 0
-    trained_with_quantization = True
+    # trained_with_quantization = True
     seen = 0
 
-    time1 = time.time()
+    # time1 = time.time()
     # for data, target in test_loader:
     for i, (data, target, paths, shapes) in enumerate(tqdm(dataloader, desc=s)):
         # print('Now calculating the {}th data...'.format(i+1))
@@ -285,3 +175,121 @@ if __name__ == '__main__':
     # Print results
     pf = '%20s' + '%12i' * 2 + '%12.3g' * 4  # print format
     print(pf % ('all', seen, nt.sum(), mp, mr, map50, map))
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='test.py')
+    parser.add_argument('--model_name', type=str, default='voc_retrain',
+                        help='which model? choose in voc_retrain | coco_origin | coco128_retrain ')
+    parser.add_argument('--data', type=str,
+                        default='data/voc.yaml', help='*.data path')
+    parser.add_argument('--batch-size', type=int, default=32,
+                        help='size of each image batch')
+
+    parser.add_argument('--task', type=str,
+                        default='test', help='train, val or test')
+
+    parser.add_argument('--one_anchor', action='store_true',
+                        help='default means use all two anchor, and add the flag means use one anchor.')
+
+    parser.add_argument('--recon_qmodel', action='store_true',
+                        help='default means load quant model,and add the flag means reconstruct quant model.')
+    parser.add_argument('--cs_self_def', action='store_true',
+                        help='calibration source:a folder including some source imgs to feed quantization model defined manually')
+    parser.add_argument('--cs_dir', type=str, default='E:/Academic_study/datasets/coco128/images/train2017',
+                        help='if cs_self_def is true, use cs_dir as calibration source')
+    parser.add_argument('--cs_use_set', type=str, default='val',
+                        help='if cs_self_def is false, use dataset as calibration source, you can choose in val and train')
+    parser.add_argument('--cs_num', type=int, default=128,
+                        help='limit img number of calibration source')
+
+    parser.add_argument('--quant_strategy', type=str, default='baseline',
+                        help='choose quant strategy in baseline/selfdefine...')
+    parser.add_argument('--save_qw', action='store_true',
+                         help='save quanted weight')
+    opt = parser.parse_args()
+    # if('recon_qmodel' in vars(opt)):
+    #     if('cs_self_def' in vars(opt)):
+    #         if('')
+    # opt.save_json |= opt.data.endswith('coco.yaml')
+    # opt.data = check_file(opt.data)  # check file
+    print(opt, type(opt))
+    warnings.filterwarnings('ignore')
+
+    model_name = opt.model_name
+    data = 'models/yolov3-tiny_voc.yaml' if model_name == 'voc_retrain' else 'models/yolov3-tiny.yaml'
+    relative_path = './models_files/' + model_name
+    float_weight = relative_path + '/weights/best.pt'
+    quant_weight = relative_path + '/yolov3tiny_quant.pth'
+
+    assert(os.path.exists(float_weight))
+    assert(os.path.exists(quant_weight))
+    print("->使用现有浮点权重：\n\t"+str(float_weight))
+    if(opt.recon_qmodel): # 重构量化模型
+        # 准备校准数据
+        if opt.cs_self_def: # 自定义校准数据
+            perpare_source = opt.cs_dir
+        else:
+            dataset_yaml = 'data/' + model_name.split('_')[0]+'.yaml'
+            with open(dataset_yaml) as f:
+                data_yaml = yaml.safe_load(f)
+            perpare_source = data_yaml[opt.cs_use_set]
+
+        print("->重构量化权重：\n\t使用校准数据："+str(perpare_source) +
+              "\n\t图片数量："+str(opt.cs_num))
+        calibrate_dataset = LoadImages(perpare_source, img_size=416, stride=32)
+        float_model = load_float_model(float_weight)
+        
+        if(opt.quant_strategy == 'baseline'): # 使用默认的量化策略
+            print("->baseline量化策略：")
+            quant_model = generate_quant_model_baseline(
+                float_model, calibrate_dataset, opt.cs_num)
+        else: # 使用自定义的量化策略，产生了量化模型
+            print("->selfdefine量化策略：")
+            quant_model = generate_quant_model_selfdefine(
+                float_model, calibrate_dataset, opt.cs_num)
+    else: # 加载保存好的量化模型
+        # load model only in default_qconfig strategy
+        quant_model = load_quant_model(float_weight, quant_weight)
+        print("加载现有量化权重："+str(quant_weight))
+
+        # 保存量化模型
+    if opt.save_qw:
+        state_dict = quant_model.state_dict()
+        torch.save(state_dict, './models_files/' +
+                   model_name + '/yolov3tiny_quant.pth')
+
+    # Configure
+    data_yaml = opt.data
+    if isinstance(data_yaml, str):
+        with open(data_yaml) as f:
+            data_cfg = yaml.safe_load(f)
+    check_dataset(data_cfg)  # check
+    nc = int(data_cfg['nc'])  # number of classes
+    iouv = torch.linspace(0.5, 0.95, 10)  # iou vector for mAP@0.5:0.95
+    niou = iouv.numel()
+    imgsz = 416
+    gs = 32
+    names = {k: v for k, v in enumerate(range(nc))}
+    anchors = [[10, 14, 23, 27, 37, 58], [81, 82, 135, 169, 344, 319]]
+    anchors = [anchors[1], anchors[0]]
+    # stride = [32, 16]
+    conf_thres = 0.001
+    iou_thres = 0.6  # for NMS
+    batch_size = opt.batch_size
+    device = 'cpu'
+    na = quant_model[1].model[-1].na
+    nd = 1 if opt.one_anchor else quant_model[1].model[-1].nl # number of detection layers
+    no = quant_model[1].model[-1].no
+    stride = quant_model[1].model[-1].stride
+    anchor = quant_model[1].model[-1].anchors
+    anchor_grid = quant_model[1].model[-1].anchor_grid
+
+    task = opt.task
+    dataloader = create_dataloader(data_cfg[task], imgsz, batch_size, gs, opt, pad=0.5, rect=True,
+                                   prefix=colorstr(f'{task}: '))[0]
+
+    quant_test(quant_model,dataloader,nc ,iouv,niou,names,conf_thres,iou_thres,device,nd,na,no,stride,anchor_grid)
+
+ 
